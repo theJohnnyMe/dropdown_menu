bf4143aed55f1383272154401ef564739f163c6d
Johnny <johnny.mirkovic@comeon.com>
2020-02-11 18:05:35 +0100

    Adding svg icons

---
+++ src/icons/icon-play.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const IconPlay = props => (
+  <svg height="50" width="50" viewBox="0 0 50 50" aria-hidden="true" {...props}>
+    <title>Icon Play</title>
+    <path d="M 10 5.25 L 10 44.75 L 11.5 43.875 L 42.09375 25.875 L 43.5625 25 L 42.09375 24.125 L 11.5 6.125 Z M 12 8.75 L 39.59375 25 L 12 41.25 Z"></path>
+  </svg>
+);
+
+export default IconPlay;
