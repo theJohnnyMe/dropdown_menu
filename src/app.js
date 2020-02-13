f89e45459a840d2f58f0e24659dc0c36c423e863
Johnny <johnny.mirkovic@comeon.com>
2020-02-13 08:49:10 +0100

    Close dropdowns if call is not coming from button element

--- src/app.js
+++ src/app.js
@@ -3,31 +3,24 @@ import "./app.scss";
 
 import Header from "./layout/header.js";
 import { HeaderButton } from "./layout/button.js";
-
-//import ActionDD from "./dropdowns/action-dd.js";
-//import MobileDD from "./dropdowns/mobile-dd.js";
-
 import NavigationDD from "./dropdowns/navigation-dd.js";
-
 import MixedDD from "./dropdowns/mixed-dd.js";
 
 class App extends React.PureComponent {
   container = React.createRef();
+
   state = {
     linksButton: false,
     mixedButtons: false
   };
   componentDidMount() {
-    document.addEventListener("click", this.handleClickOutside);
+    document.addEventListener("click", this.handleClick);
   }
   componentWillUnmount() {
-    document.removeEventListener("click", this.handleClickOutside);
+    document.removeEventListener("click", this.handleClick);
   }
-  handleClickOutside = event => {
-    if (
-      this.container.current &&
-      !this.container.current.contains(event.target)
-    ) {
+  handleClick = event => {
+    if (event.target.tagName !== "BUTTON") {
       this.setState({
         linksButton: false,
         mixedButton: false
@@ -46,9 +39,13 @@ class App extends React.PureComponent {
   render() {
     return (
       <div ref={this.container}>
-        <Header innerRef={this.container}>
-          <HeaderButton onClick={this.linksMenu}>Links menu</HeaderButton>
-          <HeaderButton onClick={this.mixedMenu}>Mixed menu</HeaderButton>
+        <Header>
+          <HeaderButton type="button" onClick={this.linksMenu}>
+            Links menu
+          </HeaderButton>
+          <HeaderButton type="button" onClick={this.mixedMenu}>
+            Mixed menu
+          </HeaderButton>
         </Header>
         {this.state.linksButton && <NavigationDD wide left="0" top="40px" />}
         {this.state.mixedButton && <MixedDD wide right="0" top="40px" />}
